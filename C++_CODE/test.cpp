289924da6716aab47bf281e0bf20d8940644b71e
fcchbjm <2748195351@qq.com>
2024-03-09 20:33:50 +0800

    添加项目文件。

---
+++ C++_CODE/test.cpp
@@ -0,0 +1,16 @@
+////C++ºÊ»›C
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello World\n");
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World" << endl;
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
