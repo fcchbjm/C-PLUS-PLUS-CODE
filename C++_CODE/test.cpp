dc4124d09e3f995a2b4f27510e6c2765100c30c5
fcchbjm <2748195351@qq.com>
2024-03-23 21:13:51 +0800

    upload

--- C++_CODE/test.cpp
+++ C++_CODE/test.cpp
@@ -6,11 +6,44 @@
 //	return 0;
 //}
 
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "Hello World" << endl;
-	std::cout << "Hello World" << std::endl;
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "Hello World" << endl;
+//	std::cout << "Hello World" << std::endl;
+//	return 0;
+//}
+
+//namespace A
+//{
+//	int Add(int x, int y)
+//	{
+//		return x + y;
+//	}
+//}
+//
+//namespace B
+//{
+//	double Add(double x, double y)
+//	{
+//		return x + y;
+//	}
+//}
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	cin >> a >> b;
+//	int ret = A::Add(a, b);
+//	cout << ret << endl;
+//	double c = 0.0;
+//	double d = 0.0;
+//	cin >> c >> d;
+//	cout << B::Add(c, d) << endl;
+//	return 0;
+//}
