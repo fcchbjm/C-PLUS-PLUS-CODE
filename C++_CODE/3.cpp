6549bf12e28e0ec6f4212840d941ce258717817b
fcchbjm <2748195351@qq.com>
2024-09-04 19:31:10 +0800

    upload

--- C++_CODE/3.cpp
+++ C++_CODE/3.cpp
@@ -1,4 +1,70 @@
+//#include <iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	void SetDate(int year, int month, int day)//成员函数共用一份空间
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	void PrintDate()
+//	{
+//		cout << _year << "/" << _month << "/" << _day << endl;
+//	}
+//private:
+//	int _year;//成员变量在实例化对象时才会开空间
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1;
+//	Date d2;
+//	d1.SetDate(2024, 9, 4);
+//	d2.SetDate(2024, 8, 31);
+//	d1.PrintDate();
+//	d2.PrintDate();
+//	return 0;
+//}
 
-
+//#include <iostream>
+//using namespace std;
+//
+//class A//类成员变量遵循内存对齐
+//{
+//public:
+//	void PrintA()
+//	{
+//		cout << _ch << " " << _i << endl;
+//	}
+//private:
+//	char _ch;
+//	int _i;
+//};
+//
+//class B//没有成员变量，给一个字节，占位标识对象存在
+//{
+//	void PrintB()
+//	{
+//		//...
+//	}
+//};
+//
+//class C
+//{
+//	//...
+//};
+//
+//int main()
+//{
+//	cout << "sizeof(A) = " << sizeof(A) << endl;
+//	cout << "sizeof(B) = " << sizeof(B) << endl;
+//	cout << "sizeof(C) = " << sizeof(C) << endl;
+//	return 0;
+//}
 
 
