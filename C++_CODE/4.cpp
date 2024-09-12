b8f43b7e7ad370e371024595cb108c2e4a28b793
fcchbjm <2748195351@qq.com>
2024-09-12 20:42:16 +0800

    upload

--- C++_CODE/4.cpp
+++ C++_CODE/4.cpp
@@ -1,35 +1,162 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	Date()
+//	{
+//		_year = 1;
+//		_month = 1;
+//		_day = 1;
+//	}
+//
+//	void SetDate(int year, int month, int day)
+//	{
+//		this->_year = year;
+//		this->_month = month;
+//		this->_day = day;
+//	}
+//
+//	//拷贝构造函数
+//	// 未显式定义拷贝构造函数，会执行浅拷贝（逐字节拷贝）
+//	Date(const Date& d)//参数必须是类类型的引用
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//
+//	void PrintDate()
+//	{
+//		cout << this->_year << "/" << this->_month << "/" << this->_day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//void func1(Date& d)
+//{
+//	cout << &d << endl;
+//	d.PrintDate();
+//}
+//
+//int main()
+//{
+//	Date d1;
+//	d1.SetDate(2024, 9, 12);
+//	func1(d1);
+//
+//	Date d2 = d1;
+//	func1(d2);
+//
+//	return 0;
+//}
 
-class Date
-{
-public:
-	void SetDate(int year, int month, int day)
-	{
-		this->_year = year;
-		this->_month = month;
-		this->_day = day;
-	}
+//#include <iostream>
+//using namespace std;
+//
+//typedef int STDataType;
+//class Stack
+//{
+//public:
+//	Stack(int n = 4)
+//	{
+//		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc failed");
+//			return;
+//		}
+//		_capacity = n;
+//		_top = 0;
+//	}
+//
+//	// st2(st1)
+//	Stack(const Stack& st)
+//	{
+//		cout << "Stack(const Stack& st)" << endl;
+//
+//		// 需要对_a指向资源创建同样大的资源再拷贝值
+//		_a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc failed");
+//			return;
+//		}
+//		memcpy(_a, st._a, sizeof(STDataType) * st._top);
+//		_top = st._top;
+//		_capacity = st._capacity;
+//	}
+//
+//	void Push(STDataType x)
+//	{
+//		if (_top == _capacity)
+//		{
+//			int newcapacity = _capacity * 2;
+//			STDataType* tmp = (STDataType*)realloc(_a, newcapacity *
+//				sizeof(STDataType));
+//			if (tmp == NULL)
+//			{
+//				perror("realloc fail");
+//				return;
+//			}
+//			_a = tmp;
+//			_capacity = newcapacity;
+//		}
+//		_a[_top++] = x;
+//
+//	}
+//	~Stack()
+//	{
+//		cout << "~Stack()" << endl;
+//		free(_a);
+//		_a = nullptr;
+//		_top = _capacity = 0;
+//	}
+//private:
+//	STDataType* _a;
+//	size_t _capacity;
+//	size_t _top;
+//};
+//
+//int main()
+//{
+//	Stack st1;
+//	st1.Push(1);
+//	st1.Push(2);
+//
+//	// Stack不显示实现拷贝构造，用自动生成的拷贝构造完成浅拷贝
+//	// 会导致st1和st2里面的_a指针指向同一块资源，析构时会析构两次，程序崩溃
+//	Stack st2(st1);
+//
+//	return 0;
+//}
 
-	Date(Date& d)
-	{
-		d._year = _year;
-		d._month = _month;
-		d._day = _day;
-	}
+//#include <iostream>
+//using namespace std;
+// 
+//class A
+//{
+//public:
+//	void func()
+//	{
+//		cout << "A::func()" << endl;
+//	}
+//};
+//
+//typedef void(A::*PF)(); //成员函数指针类型
+//
+//int main()
+//{
+//	// void(A::*pf)() = nullptr;
+//	PF pf = nullptr;
+//	// C++规定成员函数要加&才能取到函数指针
+//
+//	pf = &A::func;
+//	A aa;
+//	(aa.*pf)();//.* 操作符，隐式的回调this指针
+//}
 
-	void PrintDate()
-	{
-		cout << this->_year << "/" << this->_month << "/" << this->_day << endl;
-	}
-private:
-	int _year;
-	int _month;
-	int _day;
-};
-
-int main()
-{
-
-	return 0;
-}
