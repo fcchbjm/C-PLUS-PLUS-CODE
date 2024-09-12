9e4a3707c1179b7617770394743438bf1197888f
fcchbjm <2748195351@qq.com>
2024-09-12 21:26:39 +0800

    upload

--- C++_CODE/4.cpp
+++ C++_CODE/4.cpp
@@ -160,3 +160,199 @@
 //	(aa.*pf)();//.* 操作符，隐式的回调this指针
 //}
 
+////操作符重载
+//// 1.至少要有一个自定义类型
+//// 2.运算符重载参数个数和运算操作对象个数一样多
+////  1）一元运算符——一个参数
+////  2）二元运算符——两个参数，第一个参数对应运算符左侧，第二个参数对应运算符右侧
+//// 3.如果是成员函数，第一个参数为隐式传递的this指针，参数会少一个
+//// 4.重载后结合优先级不变，不能创建操作符
+//#include<iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	//赋值运算符重载
+//	Date& operator=(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//
+//		return *this;
+//	}
+//
+//	// 拷贝构造
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//
+//	// 构造
+//	Date(const Date* d)
+//	{
+//		_year = d->_year;
+//		_month = d->_month;
+//		_day = d->_day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "/" << _month << "/" << _day << endl;
+//	}
+//
+//	// 默认是inline
+//	int GetMonthDay(int year, int month)
+//	{
+//		static int monthDayArray[13] = { -1, 31, 28, 31, 30, 31, 30,
+//31, 31, 30, 31, 30, 31 };
+//
+//		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+//		{
+//			return 29;
+//		}
+//
+//		return monthDayArray[month];
+//	}
+//
+//	bool operator==(const Date& d)
+//	{
+//		return _year == d._year
+//			&& _month == d._month
+//			&& _day == d._day;
+//	}
+//
+//	//d1 + 100;
+//	Date operator+(int day);
+//
+//	//d1 - 100;
+//	Date operator-(int day);
+//
+//	// d1 - d2;
+//	int operator-(const Date& d);
+//
+//	Date& operator+=(int day);
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//// d1 += 100
+//Date& Date::operator+=(int day)
+//{
+//	_day += day;
+//	while (_day > GetMonthDay(_year, _month))
+//	{
+//		_day -= GetMonthDay(_year, _month);
+//		++_month;
+//		if (_month == 13)
+//		{
+//			_year++;
+//			_month = 1;
+//		}
+//	}
+//
+//	return *this;
+//}
+//
+//// d1 + 100
+//Date Date::operator+(int day)
+//{
+//	Date tmp = *this;
+//
+//	tmp += day;
+//	/*tmp._day += day;
+//	while (tmp._day > GetMonthDay(tmp._year, tmp._month))
+//	{
+//		tmp._day -= GetMonthDay(tmp._year, tmp._month);
+//		++tmp._month;
+//		if (tmp._month == 13)
+//		{
+//			tmp._year++;
+//			tmp._month = 1;
+//		}
+//	}*/
+//
+//	return tmp;
+//}
+//
+////bool operator==(const Date& d1, const Date& d2)
+////{
+////	return d1._year == d2._year
+////		&& d1._month == d2._month
+////		&& d1._day == d2._day;
+////}
+//
+//void test01()
+//{
+//	Date d1(2024, 9, 12);
+//	Date d2(2024, 9, 13);
+//
+//	// 运算符重载函数可以显示调用
+//	//operator==(d1, d2);
+//	// 编译器会转换成 operator==(d1, d2);
+//	//d1 == d2;
+//
+//	// 运算符重载函数可以显示调用
+//	d1.operator==(d2);
+//
+//	// 编译器会转换成 d1.operator==(d2);
+//	cout << (int)(d1 == d2) << endl;
+//
+//	d1 + 100;
+//	//d1 - 100;
+//	//d1 - d2;
+//}
+//
+//void test02()
+//{
+//	Date d1(2024, 9, 12);
+//	Date d2(2024, 9, 13);
+//
+//	// 赋值重载拷贝
+//	d1 = d2;
+//
+//	// 拷贝构造
+//	Date d3(d2);
+//	Date d4 = d2;
+//
+//	d4 = d3 = d1;
+//}
+//
+//void test03()
+//{
+//	Date d1(2024, 9, 12);
+//	Date d2 = d1 + 100;
+//	//Date d3(d1 + 100);
+//	d1.Print();
+//	d2.Print();
+//
+//	//d1 += 100;
+//	//d1.Print();
+//
+//	d1 += 30000;
+//	d1.Print();
+//}
+//
+//int main()
+//{
+//	test01();
+//
+//	//test02();
+//
+//	//test03();
+//
+//	return 0;
+//}
