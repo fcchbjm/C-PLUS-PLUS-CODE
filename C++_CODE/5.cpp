4a2145f06cc801f6aa7ce5950755caa93149c643
fcchbjm <2748195351@qq.com>
2024-09-22 19:17:29 +0800

    upload

--- C++_CODE/5.cpp
+++ C++_CODE/5.cpp
@@ -338,6 +338,80 @@
 //	return 0;
 //}
 
+//#include<iostream>
+//using namespace std;
+//class A
+//{
+//public:
+//	//explicit A(int a = 0)//explicit可以拒绝隐式类型转换
+//	A(int a = 0)
+//	{
+//		_a1 = a;
+//	}
+//
+//	A(const A& aa)
+//	{
+//		_a1 = aa._a1;
+//	}
+//
+//	A(int a1, int a2)
+//		:_a1(a1)
+//		, _a2(a2)
+//	{}
+//
+//	void Print() {
+//		cout << _a1 << " " << _a2 << endl;
+//	}
+//private:
+//	int _a1;
+//	int _a2;
+//};
+//
+//class Stack
+//{
+//public:
+//	void Push(const A& aa)
+//	{
+//		//...
+//	}
+//private:
+//	A _arr[10];
+//	int _top;
+//};
+//
+//int main()
+//{
+//	A aa1(1);
+//	aa1.Print();
+//
+//	// 隐式类型转换
+//	// 2构造一个A的临时对象，再用这个临时对象拷贝构造aa2
+//	// 编译器遇到连续构造+拷贝构造->优化为直接构造
+//	A aa2 = 2;
+//	aa2.Print();
+//
+//	A& raa1 = aa2;
+//	const A& raa2 = 2;//临时变量具有常性
+//
+//	int i = 1;
+//	double d = i;
+//	const double& rd = i;
+//
+//	Stack st;
+//
+//	A aa3(3);
+//	st.Push(aa3);
+//
+//	st.Push(3);//单参数构造函数可以隐式类型转换
+//
+//	// C++11之后支持多参数构造函数完成隐式类型转换
+//	A aa5 = { 1, 1 };
+//	const A& raa6 = { 2,2 };
+//	st.Push(aa5);
+//	st.Push({ 2,2 });
+//
+//	return 0;
+//}
 
 
 
