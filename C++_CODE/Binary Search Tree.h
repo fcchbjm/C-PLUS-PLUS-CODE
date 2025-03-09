072c1b229eb849ebdb667c543a6c72ea353b90db
陈晨 方 <2748195351@qq.com>
2025-03-09 21:37:54 +0800

    upload

--- C++_CODE/Binary Search Tree.h
+++ C++_CODE/Binary Search Tree.h
@@ -19,7 +19,31 @@ class BSTree
 	//typedef BSTNode<K> Node;
 	using Node = BSTNode<K>;
 public:
+	bool Insert(const K& key)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(key);
+			return true;
+		}
 
+		Node* parent = nullptr;
+		Node* cur = _root;
+
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+		}
+	}
 private:
 	Node* _root = nullptr;
 };
