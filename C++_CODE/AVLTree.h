40311029359cd70886c5a50898889cf3cc814dc3
陈晨 方 <2748195351@qq.com>
2025-04-21 21:57:55 +0800

    upload

---
+++ C++_CODE/AVLTree.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	pair<K, V> _kv;
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;//更新平衡因子
+	int _bf;//balance factor
+	AVLTreeNode(const pair<K, V>& kv)
+		:_kv(kv)
+		,_left(nullptr)
+		,_right(nullptr)
+		,_parent(nullptr)
+		,_bf(0)
+	{}
+};
+
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+
+		while (cur)
+		{
+			if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else (parent->_kv->first > kv.first)
+		{
+			parent->_left = cur;
+		}
+		cur->_parent = parent;
+
+		return true;
+	}
+private:
+	Node* _root;
+};
